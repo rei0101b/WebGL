66646a51f51686f2b459f66119c700c981b7d196
rei0101b <rei0101b@gmail.com>
2019-09-15 00:03:39 +0900

    initial commit

---
+++ main.js
@@ -0,0 +1,19 @@
+onload = function() {
+  var c = document.getElementById("canvas");
+
+  c.width = 500;
+  c.height = 300;
+
+  var gl = c.getContext("webgl") || c.getContext("experimental-webgl");
+
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+};
+
+function create_shader(id) {
+  var shader;
+  var scriptElement = document.getElementById(id);
+  if (!scriptElemtn) {
+    return;
+  }
+}
