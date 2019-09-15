7e0b61d9f91fa6633265b6cbf48467418c9e97f7
rei0101b <rei0101b@gmail.com>
2019-09-16 00:28:21 +0900

    複数レンダリング

--- main.js
+++ main.js
@@ -38,35 +38,45 @@ onload = function() {
     1.0
   ];
 
-  var position_vbo = create_vbo(vertex_position);
-  var color_vbo = create_vbo(vertex_color);
-
-  gl.bindBuffer(gl.ARRAY_BUFFER, position_vbo);
-  gl.enableVertexAttribArray(attLocation[0]);
-  gl.vertexAttribPointer(attLocation[0], attStride[0], gl.FLOAT, false, 0, 0);
-
-  gl.bindBuffer(gl.ARRAY_BUFFER, color_vbo);
-  gl.enableVertexAttribArray(attLocation[1]);
-  gl.vertexAttribPointer(attLocation[1], attStride[1], gl.FLOAT, false, 0, 0);
+  pos_vbo = create_vbo(vertex_position);
+  co_vbo = create_vbo(vertex_color);
+  set_attribute([pos_vbo, co_vbo], attLocation, attStride);
 
   var m = new matIV();
-
   var mMatrix = m.identity(m.create());
   var vMatrix = m.identity(m.create());
   var pMatrix = m.identity(m.create());
+  var tmpMatrix = m.identity(m.create());
   var mvpMatrix = m.identity(m.create());
-
-  m.lookAt([0.0, 1.0, 3.0], [0, 0, 0], [0, 1, 0], vMatrix);
-
-  m.perspective(90, c.width / c.height, 0.1, 100, pMatrix);
-
-  m.multiply(pMatrix, vMatrix, mvpMatrix);
-  m.multiply(mvpMatrix, mMatrix, mvpMatrix);
 
   var uniLocation = gl.getUniformLocation(prg, "mvpMatrix");
 
-  gl.uniformMatrix4fv(uniLocation, false, mvpMatrix);
+  m.lookAt([0.0, 1.0, 3.0], [0, 0, 0], [0, 1, 0], vMatrix);
+  m.perspective(90, c.width / c.height, 0.1, 100, pMatrix);
+  m.multiply(pMatrix, vMatrix, tmpMatrix);
 
+  m.translate(mMatrix, [1.5, 0.0, 0.0], mMatrix);
+  m.multiply(tmpMatrix, mMatrix, mvpMatrix);
+
+  gl.uniformMatrix4fv(uniLocation, false, mvpMatrix);
+  gl.drawArrays(gl.TRIANGLES, 0, 3);
+
+  m.identity(mMatrix);
+  m.translate(mMatrix, [-1.5, 0.0, 0.0], mMatrix);
+  m.multiply(tmpMatrix, mMatrix, mvpMatrix);
+  gl.uniformMatrix4fv(uniLocation, false, mvpMatrix);
+  gl.drawArrays(gl.TRIANGLES, 0, 3);
+
+  m.identity(mMatrix);
+  m.translate(mMatrix, [-1.5, -1.0, 0.0], mMatrix);
+  m.multiply(tmpMatrix, mMatrix, mvpMatrix);
+  gl.uniformMatrix4fv(uniLocation, false, mvpMatrix);
+  gl.drawArrays(gl.TRIANGLES, 0, 3);
+
+  m.identity(mMatrix);
+  m.translate(mMatrix, [0.5, -0.2, 2.2], mMatrix);
+  m.multiply(tmpMatrix, mMatrix, mvpMatrix);
+  gl.uniformMatrix4fv(uniLocation, false, mvpMatrix);
   gl.drawArrays(gl.TRIANGLES, 0, 3);
 
   gl.flush();
@@ -122,5 +132,20 @@ onload = function() {
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(data), gl.STATIC_DRAW);
     gl.bindBuffer(gl.ARRAY_BUFFER, null);
     return vbo;
+  }
+
+  function set_attribute(vbo, attL, attS) {
+    for (var i in vbo) {
+      gl.bindBuffer(gl.ARRAY_BUFFER, vbo[i]);
+      gl.enableVertexAttribArray(attLocation[i]);
+      gl.vertexAttribPointer(
+        attLocation[i],
+        attStride[i],
+        gl.FLOAT,
+        false,
+        0,
+        0
+      );
+    }
   }
 };
